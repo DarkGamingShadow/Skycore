a7dd14dfb5a97687b25f3a554ec3a19e9ac8ef13
DarkGamingShadow <Der_MasterLP@t-online.de>
2026-06-26 00:03:46 +0200

    Start

---
+++ src/client/java/de/dgs/skycore/client/SkycoreClient.java
@@ -0,0 +1,10 @@
+package de.dgs.skycore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkycoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/dgs/skycore/Skycore.java
@@ -0,0 +1,10 @@
+package de.dgs.skycore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Skycore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
