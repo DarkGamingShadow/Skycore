130e76a0379c2f5a2283b6ed61b471b3120aa9f7
DarkGamingShadow <Der_MasterLP@t-online.de>
2026-06-26 02:24:32 +0200

    "Skycore works" Logger in Client

--- src/client/java/de/dgs/skycore/client/SkycoreClient.java
+++ src/client/java/de/dgs/skycore/client/SkycoreClient.java
@@ -1,10 +1,15 @@
 package de.dgs.skycore.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SkycoreClient implements ClientModInitializer {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger("skycore");
+
     @Override
     public void onInitializeClient() {
+        LOGGER.info("Skycore works");
     }
 }
